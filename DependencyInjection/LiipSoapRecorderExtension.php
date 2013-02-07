b91bdb8053e7d0c415d4ac7282b939a02b2a4b00
pierrevanhulst <pierre.vanhulst@unifr.ch>
2013-02-07 09:41:03 +0100

    Adding the Data Collector for Symfony Profiler.

--- DependencyInjection/LiipSoapRecorderExtension.php
+++ DependencyInjection/LiipSoapRecorderExtension.php
@@ -3,6 +3,8 @@
 namespace Liip\SoapRecorderBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
@@ -17,5 +19,10 @@ class LiipSoapRecorderExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         $container->setParameter('liip_soap_recorder_config', $config);
+                
+        if($container->getParameter('liip_soap_recorder_config')['enable_profiler']) {        
+            $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+            $loader->load('profiler.xml');
+        }
     }
 }
